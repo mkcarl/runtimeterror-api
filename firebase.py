392be8adf87524b0d802389126212739ff9d71d9
mkcarl <munkye29@gmail.com>
2023-07-26 16:26:08 +0800

    updated firebase.py to use os.getenv

--- firebase.py
+++ firebase.py
@@ -5,17 +5,17 @@ import os
 load_dotenv()
 
 cert = credentials.Certificate({
-    "type": os.environ.get('FIREBASE_TYPE'),
-    "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
-    "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
-    "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
-    "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
-    "client_id": os.environ.get('FIREBASE_CLIENT_ID'),
-    "auth_uri": os.environ.get('FIREBASE_AUTH_URI'),
-    "token_uri": os.environ.get('FIREBASE_TOKEN_URI'),
-    "auth_provider_x509_cert_url": os.environ.get('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
-    "client_x509_cert_url": os.environ.get('FIREBASE_CLIENT_X509_CERT_URL'),
-    "universe_domain": os.environ.get('FIREBASE_UNIVERSE_DOMAIN')
+    "type": os.getenv('FIREBASE_TYPE'),
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
+    "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN')
 })
 firebaseApp = initialize_app(cert)
 
