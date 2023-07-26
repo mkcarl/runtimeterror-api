d1741dfefb7e65f7fa12557b965ca90834d35349
mkcarl <munkye29@gmail.com>
2023-07-26 16:08:15 +0800

    updated firebase.py to read from .env

--- firebase.py
+++ firebase.py
@@ -1,7 +1,22 @@
 from firebase_admin import initialize_app, firestore, credentials
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-cert = credentials.Certificate('service_account.json')
+cert = credentials.Certificate({
+    "type": os.environ.get('FIREBASE_TYPE'),
+    "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.environ.get('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.environ.get('FIREBASE_AUTH_URI'),
+    "token_uri": os.environ.get('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.environ.get('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.environ.get('FIREBASE_CLIENT_X509_CERT_URL'),
+    "universe_domain": os.environ.get('FIREBASE_UNIVERSE_DOMAIN')
+})
 firebaseApp = initialize_app(cert)
 
 firestore = firestore.client(firebaseApp)
